f04ab94ecca254e3f822e484ce20a38220f98a4b
jkarrer <jkarrer@protect247.com>
2024-04-03 11:50:56 -0400

    wip: email confirmation is working

--- src/infra/router.ts
+++ src/infra/router.ts
@@ -12,17 +12,18 @@ const Router = new Elysia()
   .decorate("auth", db)
   .get("/", ({ request, auth }) => Bun.file("public/index.html"))
   .get("/auth", () => AuthController.render_ui())
+  .get("/auth/welcome", () => "<h1>Please Confirm Email</h1>")
   .get("/auth/confirm_email", async ({ request, auth }) => {
     let check = await AuthController.email_confirmation_attempt(request, auth);
     console.log(check);
     if (check.success) {
       let headers = new Headers();
       const maxAge = 100 * 365 * 24 * 60 * 60; // 100 years, never expires
-      headers.set(
+      headers.append(
         "Set-Cookie",
         `access_token=${check.message.access_token}; path=/; SameSite=Strict; HttpOnly; max-age=${maxAge}`
       );
-      headers.set(
+      headers.append(
         "Set-Cookie",
         `refresh_token=${check.message.refresh_token}; path=/; SameSite=Strict; HttpOnly; max-age=${maxAge}`
       );
@@ -49,7 +50,7 @@ const Router = new Elysia()
     let register_attempt = await AuthController.register_user(request, auth);
     console.log(register_attempt);
     if (register_attempt.success) {
-      return Response.redirect("./");
+      return Response.redirect("./welcome");
     } else {
       return Response.redirect(".");
     }
